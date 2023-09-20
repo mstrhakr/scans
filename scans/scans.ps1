f5aa2f5962f3e82ea640379ace79596ab578797c
Nick Szittai <nszittai@gmail.com>
2023-09-20 13:41:10 -0400

    cleanup scans.ps1

--- scans/scans.ps1
+++ scans/scans.ps1
@@ -1,26 +1,26 @@
 #Setup scan user variables
-$user = "scans"
-$password = "scans" | ConvertTo-SecureString -asPlainText -Force
+$user = Read-Host -Prompt "Enter username"
+$password = Read-Host -Prompt "Enter Password" -AsSecureString
+# $password = "scans" | ConvertTo-SecureString -asPlainText -Force
+$txt = "Scan user created by PSP."
 
 #Creates scans user account if it doesn't exist, otherwise sets password for account
 if(![boolean](Get-LocalUser -Name $user -ErrorAction Ignore)) {
 	Write-Debug "Adding scans account"
-	New-LocalUser -Name $user -Password $password -Description "scans" -AccountNeverExpires -PasswordNeverExpires -UserMayNotChangePassword -FullName "scans" -ErrorAction Ignore | Out-Null
+	New-LocalUser -Name $user -Password $password -Description $txt -AccountNeverExpires -PasswordNeverExpires -UserMayNotChangePassword -FullName "scans" -ErrorAction Ignore | Out-Null
 } else {
 	Write-Debug "Setting up scans account"
-	Set-LocalUser -Name $user -Password $password -Description "scans" -ErrorAction Ignore | Out-Null
+	Set-LocalUser -Name $user -Password $password -Description $txt -ErrorAction Ignore | Out-Null
 }
 
 
 #Hide scans account from login screen on non domain joined computers
 if(!(Get-CimInstance -ClassName Win32_ComputerSystem).PartOfDomain){	
 	$path = 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\Userlist'
-	$value = 0
-	$name = $user
 	if(!(Test-Path $path)){
 		New-Item -Path $path -Force
 	}
-	New-ItemProperty -path $path -name $name -value $value -PropertyType 'DWord' -Force | Out-Null
+	New-ItemProperty -path $path -name $user -value 0 -PropertyType 'DWord' -Force | Out-Null
 }
 
 #Check if scans folder exists, create if missing
