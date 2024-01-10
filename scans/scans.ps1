21abd28b8e923a770299f2a09dbd13ddc57d3d90
Nick Szittai <nszittai@gmail.com>
2024-01-10 13:21:10 -0500

    Fixed folder creation, added output for testing

--- scans/scans.ps1
+++ scans/scans.ps1
@@ -9,8 +9,10 @@ param (
 
 # Creates scans user account if it doesn't exist, otherwise sets password for account
 if(![boolean](Get-LocalUser -Name $username -ErrorAction Ignore)) {
+	Write-Output "Creating New User: $username"
 	New-LocalUser -Name $username -Password $($password | ConvertTo-SecureString -AsPlainText -Force) -Description $description -AccountNeverExpires -PasswordNeverExpires -UserMayNotChangePassword -FullName "scans" -ErrorAction Ignore | Out-Null;
 } else {
+	Write-Output "Updating User: $username"
 	Set-LocalUser -Name $username -Password $($password | ConvertTo-SecureString -AsPlainText -Force) -Description $description -ErrorAction Ignore | Out-Null;
 }
 
@@ -18,16 +20,22 @@ if(![boolean](Get-LocalUser -Name $username -ErrorAction Ignore)) {
 # Hide scans account from login screen on non domain joined computers
 $computerDetails = Get-CimInstance -ClassName Win32_ComputerSystem
 if(!$computerDetails.PartOfDomain){	
+	Write-Output "Computer is part not domain joined"
 	$path = 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\Userlist';
 	if(!(Test-Path $path)){
+		Write-Output "Creating Registry Object"
 		New-Item -Path $path -Force | Out-Null;
 	}
+	Write-Output "Hiding scans user from login screen"
 	New-ItemProperty -path $path -name $username -value 0 -PropertyType 'DWord' -Force | Out-Null;
 }
 
 # Check if scans folder exists, create if missing
 if(!(Test-Path -Path $folderPath)){
-    New-Item -Path $folderPath.Split(':')[0] + ':' -Name $folderPath.Split(':')[1] -ItemType Directory | Out-Null;
+	Write-Output "Scans folder doesn't exist, creating"
+    New-Item -Path $($folderPath.Split(':')[0] + ':/') -Name $folderPath.Split(':')[1] -ItemType Directory | Out-Null;
+} else {
+	Write-Output "Scans folder already exists"
 }
 
 # Grant full recursive permissions on the scan folder to the scan user and all local users
@@ -40,12 +48,15 @@ if((Get-CimInstance -ClassName Win32_ComputerSystem).PartOfDomain){
 	$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Domain Users", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
 	$folderAcl.SetAccessRule($rule)
 }
+Write-Output "Setting folder permissions"
 Set-Acl $folderPath $folderAcl
 
 # Check if scans share exists, create if missing
 if(!((Get-SmbShare).Name).toLower().Contains($shareName)){
+	Write-Output "Creating SMB share"
     New-SmbShare -Name $shareName -Path $folderPath -FullAccess $username | Out-Null;
 } else {
+	Write-Output "Updating SMB share permissions"
     Grant-SmbShareAccess -Name $shareName -AccountName $username -AccessRight Full -Force | Out-Null;
 }
 
@@ -67,18 +78,26 @@ if ($build -lt 22000) {
 }
 
 $desktopShortCut.Description = $description;
+Write-Output "Creating Desktop Shortcut"
 $desktopShortCut.Save() | Out-Null;
 
 # Set network profile to Private if not domain joined.
 if(!(Get-CimInstance -ClassName Win32_ComputerSystem).PartOfDomain){
+	Write-Output "Setting Net Connection Profile to Private"
 	Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
 } else {
+	Write-Output "Setting Net Connection Profile to Domain Authenticated"
 	Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory DomainAuthenticated
 }
 
+Write-Output "Setting clipboard to computer name"
+Set-Clipboard $computerDetails.Name
+
 # Notify user about setup
+Write-Output "Notifing user of scanning details"
 $shellObject.Popup("Scanning setup is complete.
 Computer Name: $($computerDetails.Name)
+*Computer name saved to clipboard*
 Username: $username
 Password: $password
 SMB Share: $shareName
