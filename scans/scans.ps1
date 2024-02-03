42b16e174535fde79568917d364c59a825d438a0
Nick Szittai <nszittai@gmail.com>
2024-02-02 23:09:07 -0500

    v1.3.0.0 created readme, added custom password prompt, updated icon

--- scans/scans.ps1
+++ scans/scans.ps1
@@ -1,26 +1,124 @@
-# Setup parameters and defaults
-param (
-	[string]$username = "scans",
-	[string]$password = "scans",
-	[string]$folderPath = "C:\scans",
-	[string]$shareName = "scans",
-	[string]$description = "Scanning setup by PSP."
-)
+#v1.3.0.0
+# Setup variables and defaults
+[string]$username = "scans"
+[string]$password = "scans"
+[string]$folderPath = "C:\scans"
+[string]$shareName = "scans"
+[string]$description = "Scanning setup by PSP."
+
+
+# Load the .NET Framework classes
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+Add-Type -AssemblyName System.Web
+
+# Create a new form with a title and a size
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Password Preference'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+# Create a label with the prompt message and add it to the form
+$label = New-Object System.Windows.Forms.Label
+$label.Text = 'Please choose one of the password options below:'
+$label.Location = New-Object System.Drawing.Point(10,10)
+$label.AutoSize = $true
+$form.Controls.Add($label)
+
+# Create a button for option 1 and add it to the form
+$button1 = New-Object System.Windows.Forms.Button
+$button1.Text = 'Just Use "scans"'
+$button1.Location = New-Object System.Drawing.Point(75,50)
+$button1.Size = New-Object System.Drawing.Size(150,25)
+$form.Controls.Add($button1)
+
+# Create a button for option 2 and add it to the form
+$button2 = New-Object System.Windows.Forms.Button
+$button2.Text = 'Use Custom Password'
+$button2.Location = New-Object System.Drawing.Point(75,100)
+$button2.Size = New-Object System.Drawing.Size(150,25)
+$form.Controls.Add($button2)
+
+# Define what happens when the user clicks on the buttons
+$button1.Add_Click({
+    Write-Host 'The password will be set to "scans"'
+    $form.Close()
+})
+
+$button2.Add_Click({
+    $form.Close()
+	# Create a new form with a title and a size
+	$form = New-Object System.Windows.Forms.Form
+	$form.Text = 'Enter new password'
+	$form.Size = New-Object System.Drawing.Size (300,200)
+	$form.StartPosition = 'CenterScreen'
+
+	# Create a label with the input message and add it to the form
+	$label = New-Object System.Windows.Forms.Label
+	$label.Text = 'Please enter the new password:'
+	$label.Location = New-Object System.Drawing.Point (10,10)
+	$label.AutoSize = $true
+	$form.Controls.Add($label)
+
+	# Create a text box for the user input and add it to the form
+	$textBox = New-Object System.Windows.Forms.TextBox
+	$textBox.Location = New-Object System.Drawing.Point (10,40)
+	$textBox.Size = New-Object System.Drawing.Size (260,20)
+	$textBox.Text = [System.Web.Security.Membership]::GeneratePassword(10, 0)
+	$form.Controls.Add($textBox)
+
+	# Create an OK button and add it to the form
+	$okButton = New-Object System.Windows.Forms.Button
+	$okButton.Location = New-Object System.Drawing.Point (75,120)
+	$okButton.Size = New-Object System.Drawing.Size (75,23)
+	$okButton.Text = 'OK'
+	$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+	$form.AcceptButton = $okButton
+	$form.Controls.Add($okButton)
+
+	# Create a Cancel button and add it to the form
+	$cancelButton = New-Object System.Windows.Forms.Button
+	$cancelButton.Location = New-Object System.Drawing.Point (150,120)
+	$cancelButton.Size = New-Object System.Drawing.Size (75,23)
+	$cancelButton.Text = 'Use "scans"'
+	$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+	$form.CancelButton = $cancelButton
+	$form.Controls.Add($cancelButton)
+
+	# Show the form and wait for the user input
+	$form.Topmost = $true
+	$form.Add_Shown({$textBox.Select()})
+	$result = $form.ShowDialog()
+
+	# Check the result and get the text input
+	if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+		# The user clicked OK
+		$script:password = $textBox.Text
+		Write-Host "Scans Password: $($textBox.Text)"
+	} else {
+		# The user clicked Cancel
+		Write-Host "You canceled the input box: using scans as password"
+	}
+    $form.Close()
+})
+
+# Show the form and wait for the user input
+$form.ShowDialog()
 
 # Creates scans user account if it doesn't exist, otherwise sets password for account
-if(![boolean](Get-LocalUser -Name $username -ErrorAction Ignore)) {
+if(![boolean](Get-LocalUser -Name $username -ErrorAction SilentlyContinue)) {
 	Write-Output "Creating New User: $username"
-	New-LocalUser -Name $username -Password $($password | ConvertTo-SecureString -AsPlainText -Force) -Description $description -AccountNeverExpires -PasswordNeverExpires -UserMayNotChangePassword -FullName "scans" -ErrorAction Ignore | Out-Null;
+	New-LocalUser -Name $username -Password $($password | ConvertTo-SecureString -AsPlainText -Force) -Description $description -AccountNeverExpires -PasswordNeverExpires -UserMayNotChangePassword -FullName "scans";
 } else {
 	Write-Output "Updating User: $username"
-	Set-LocalUser -Name $username -Password $($password | ConvertTo-SecureString -AsPlainText -Force) -Description $description -ErrorAction Ignore | Out-Null;
+	Set-LocalUser -Name $username -Password $($password | ConvertTo-SecureString -AsPlainText -Force) -Description $description -AccountNeverExpires -PasswordNeverExpires $true -UserMayChangePassword $false -FullName "scans";
 }
 
 
 # Hide scans account from login screen on non domain joined computers
 $computerDetails = Get-CimInstance -ClassName Win32_ComputerSystem
 if(!$computerDetails.PartOfDomain){	
-	Write-Output "Computer is part not domain joined"
+	Write-Output "Computer is not domain joined"
 	$path = 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\Userlist';
 	if(!(Test-Path $path)){
 		Write-Output "Creating Registry Object"
@@ -32,7 +130,7 @@ if(!$computerDetails.PartOfDomain){
 
 # Check if scans folder exists, create if missing
 if(!(Test-Path -Path $folderPath)){
-	Write-Output "Scans folder doesn't exist, creating"
+	Write-Warning "Scans folder doesn't exist, creating"
     New-Item -Path $($folderPath.Split(':')[0] + ':/') -Name $folderPath.Split(':')[1] -ItemType Directory | Out-Null;
 } else {
 	Write-Output "Scans folder already exists"
@@ -42,9 +140,9 @@ if(!(Test-Path -Path $folderPath)){
 $folderAcl = (Get-Acl $folderPath)
 $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($username, "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
 $folderAcl.SetAccessRule($rule)
-$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Users", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
 $folderAcl.SetAccessRule($rule)
-if((Get-CimInstance -ClassName Win32_ComputerSystem).PartOfDomain){
+if($computerDetails.PartOfDomain){
 	$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Domain Users", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
 	$folderAcl.SetAccessRule($rule)
 }
@@ -82,7 +180,7 @@ Write-Output "Creating Desktop Shortcut"
 $desktopShortCut.Save() | Out-Null;
 
 # Set network profile to Private if not domain joined.
-if(!(Get-CimInstance -ClassName Win32_ComputerSystem).PartOfDomain){
+if(!$computerDetails.PartOfDomain){
 	Write-Output "Setting Net Connection Profile to Private"
 	Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
 } else {
@@ -90,15 +188,23 @@ if(!(Get-CimInstance -ClassName Win32_ComputerSystem).PartOfDomain){
 	Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory DomainAuthenticated
 }
 
-Write-Output "Setting clipboard to computer name"
-Set-Clipboard $computerDetails.Name
+
+if ($password -eq "scans"){
+	$clipboard = 'Copied computer name to clipboard'
+	Set-Clipboard $computerDetails.Name
+} else {
+	$clipboard = "Copied custom password to clipboard"
+	Set-Clipboard $password
+}
 
 # Notify user about setup
 Write-Output "Notifing user of scanning details"
 $shellObject.Popup("Scanning setup is complete.
-Computer Name: $($computerDetails.Name)
-*Computer name saved to clipboard*
+Desktop shortcut has been created.
+
 Username: $username
 Password: $password
-SMB Share: $shareName
-Local Dir: $folderPath") | Out-Null;
+Remote Dir: \\$($computerDetails.Name)\$shareName
+Local Dir: $folderPath
+
+$clipboard") | Out-Null;
